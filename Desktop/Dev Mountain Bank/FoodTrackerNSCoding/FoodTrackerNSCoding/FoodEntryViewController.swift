dd4396cb2ed055ca0844fbc1700924d2cf9efb95
reich452 <reich452@regis.edu>
2017-03-20 14:04:13 -0600

    Added MVC files and storyboard

---
+++ Desktop/Dev Mountain Bank/FoodTrackerNSCoding/FoodTrackerNSCoding/FoodEntryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FoodEntryViewController.swift
+//  FoodTrackerNSCoding
+//
+//  Created by Nick Reichard on 3/20/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class FoodEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+
+}
